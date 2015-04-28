c9ff67be5424428191c1cd9578d680080fe3b124
George <zloi.tatarin@yahoo.com>
2015-04-28 15:36:33 -0700

    Organize javascript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-
-
     $("#start-walking-button").click(function() {
         $(".before-start").fadeOut();
     });
-                
+
     $("#end-button").click(function() {
         $(".before-start").fadeIn("fast");
     });
+
+    // Chat screen animations, fading the keyboard in and out.
 
     $("#chat_reply").click(function() {
         if (!$("#chat_messages-container").hasClass("selected")) {
@@ -15,6 +15,15 @@ $(document).ready(function() {
             $("#chat_messages-container").animate({top: "-=127px"});
             $("#chat_reply-container").animate({top: "-=275px"});
             $(".chat_row#first").fadeTo("fast", 0);
+        }
+    });
+
+    $("#chat_messages-container").click(function() {
+        if ($("#chat_messages-container").hasClass("selected")) {
+            $("#chat_messages-container").removeClass("selected");
+            $("#chat_messages-container").animate({top: "+=127px"});
+            $("#chat_reply-container").animate({top: "+=275px"});
+            $(".chat_row#first").css("zIndex", -1).fadeTo("fast", 1);
         }
     });
 })
