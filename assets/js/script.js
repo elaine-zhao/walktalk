83ae4a73ef9223c1c716d88f7beeb54a46dbdf63
elaine-zhao <elainezhao@berkeley.edu>
2015-05-04 18:08:39 -0700

    changed new match msg and pic to be yellow, fixed keyboard sliding text not moving, made new thread screen for oski2, removed forgot password button, removed back button sorta (have to fix navbar)

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,17 @@ $(document).ready(function() {
 
     $("#chat_reply").click(function() {
         if (!$("chat_messages").hasClass("selected")) {
-            $("#under-topbar-container").addClass("selected");
-            $("#under-topbar-container").animate({top: "-=127px"});
+            $(".under-topbar-container").addClass("selected");
+            $(".under-topbar-container").animate({top: "-=127px"});
             $("#chat_reply-container").animate({top: "-=275px"});
             $(".chat_row#first").fadeTo("fast", 0);
         }
     });
 
     $("#under-topbar-container").click(function() {
-        if ($("#under-topbar-container").hasClass("selected")) {
-            $("#under-topbar-container").removeClass("selected");
-            $("#under-topbar-container").animate({top: "+=127px"});
+        if ($(".under-topbar-container").hasClass("selected")) {
+            $(".under-topbar-container").removeClass("selected");
+            $(".under-topbar-container").animate({top: "+=127px"});
             $("#chat_reply-container").animate({top: "+=275px"});
             $(".chat_row#first").css("zIndex", -1).fadeTo("fast", 1);
         }
